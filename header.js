7eb7621e61e71d3446ab41d65f43ec65d8c97ae2
clouderror <michajansen074@gmail.com>
2025-06-19 23:23:22 +0200

    Eerste commit

---
+++ header.js
@@ -0,0 +1,13 @@
+<header class="monet-header">
+  <div class="header-content">
+    <h1>Pixels</h1>
+    <nav>
+      <ul>
+        <li><a href="#">Home</a></li>
+        <li><a href="recepten.html">Recepten</a></li>
+        <li><a href="#">Spelletjes</a></li>
+        <li><a href="#">Clicks</a></li>
+      </ul>
+    </nav>
+  </div>
+</header>
