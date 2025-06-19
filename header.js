ba3a7153b5286b78ca9a17972696227ced82e116
clouderror <michajansen074@gmail.com>
2025-06-20 00:04:00 +0200

    2

--- header.js
+++ header.js
@@ -3,10 +3,10 @@
     <h1>Pixels</h1>
     <nav>
       <ul>
-        <li><a href="#">Home</a></li>
+        <li><a href="index.html">Home</a></li>
         <li><a href="recepten.html">Recepten</a></li>
-        <li><a href="#">Spelletjes</a></li>
-        <li><a href="#">Clicks</a></li>
+        <li><a href="spelletjes.html">Spelletjes</a></li>
+        <li><a href="clicks.html">Clicks</a></li>
       </ul>
     </nav>
   </div>
